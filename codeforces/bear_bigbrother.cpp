993c67eb281240a8d5b636552a92d1148402eef6
Laryssa Ferreira <laryssa.oliveira1902@gmail.com>
2025-10-10 15:17:59 -0300

    day 3

--- codeforces/bear_bigbrother.cpp
+++ codeforces/bear_bigbrother.cpp
@@ -1,3 +1,19 @@
-//
-// Created by Laryssa Oliveira on 10/10/25.
-//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int l, b;
+    cin >> l >> b;
+
+    int y = 0;
+
+    while (l <= b) {
+        l *= 3;
+        b *= 2;
+        y++;
+    }
+
+    cout << y << endl;
+
+    return 0;
+}
--- codeforces/petya_strings.cpp
+++ codeforces/petya_strings.cpp
@@ -1,3 +1,24 @@
-//
-// Created by Laryssa Oliveira on 10/10/25.
-//
+#include <bits/stdc++.h>
+using namespace std;
+
+void to_lower(string &s) { // for reference
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+}
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    to_lower(s1);
+    to_lower(s2 );
+
+    if (s1 < s2) {
+        cout << -1 << endl;
+    } else if (s1 > s2) {
+        cout << 1 << endl;
+    } else {
+        cout << 0 << endl;
+    }
+
+    return 0;
+
+}
